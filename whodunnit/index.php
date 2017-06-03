7166a07b420214f634f22b44111e39278a2566f8
Hallarian <coreyhensley@Coreys-MBP.fios-router.home>
2017-06-03 16:53:14 -0400

    Php Header and Footer Includes
    For Phil Sinnatra to look over

---
+++ whodunnit/index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+	<head>
+    	<meta charset="utf-8">
+
+    	<title>Who Dunnit Kentuckiana</title>
+
+  		<link rel="stylesheet" type="text/css" href="stylesheet.css">
+
+	</head>
+ 	<body>
+
+ 		<div id="main">
+		<?php include 'includes/_navHead.php'; ?>
+
+			    <div class="blackBox">
+      <h2>Current &amp; Upcoming Shows</h2>
+
+    <!--
+   _____       _ _                 
+  / ____|     | | |                
+ | |  __  __ _| | | ___ _ __ _   _ 
+ | | |_ |/ _` | | |/ _ \ '__| | | |
+ | |__| | (_| | | |  __/ |  | |_| |
+  \_____|\__,_|_|_|\___|_|   \__, |
+                              __/ |
+                             |___/ -->
+      <div class="galleryContain">
+        <div class="gallery">
+          <a target="_blank" href="imagery/A_False_Alibi.jpg">
+            <img src="imagery/A_False_Alibi.jpg" alt="A_False_Alibi">
+          </a>
+          <div class="desc">A False Alibi</div>
+        </div>
+
+        <div class="gallery">
+          <a target="_blank" href="imagery/Alien_dance.jpg">
+            <img src="imagery/Alien_dance.jpg" alt="Alien_dance">
+          </a>
+          <div class="desc">Alien Dance</div>
+        </div>
+
+        <div class="gallery">
+          <a target="_blank" href="imagery/Dark_Veil_clip.jpg">
+            <img src="imagery/Dark_Veil_clip.jpg" alt="The Dark_Veil">
+          </a>
+          <div class="desc">The<br>Dark Veil</div>
+        </div>
+
+        <div class="gallery">
+          <a target="_blank" href="imagery/Lenny's-for-web.jpg">
+            <img src="imagery/Lenny's-for-web.jpg" alt="Lenny's Place">
+          </a>
+          <div class="desc">Lenny's Place</div>
+        </div>
+
+        <div class="gallery">
+          <a target="_blank" href="imagery/Blackwood_Hall.jpg">
+            <img src="imagery/Blackwood_Hall.jpg" alt="Blackwood_Hall">
+          </a>
+          <div class="desc">The Hall of Blackwood</div>
+        </div>
+
+        <div class="gallery">
+          <a target="_blank" href="imagery/FareweillSt.jpg">
+            <img src="imagery/FareweillSt.jpg" alt="FareweillSt">
+          </a>
+          <div class="desc">The Death of Fareweill</div>
+        </div>
+
+        <div class="gallery">
+          <a target="_blank" href="imagery/MacC_and_Sherlock.jpg">
+            <img src="imagery/MacC_and_Sherlock.jpg" alt="Mac and Sherlock">
+          </a>
+          <div class="desc">Mac and Sherlock</div>
+        </div>
+
+        <div class="gallery">
+          <a target="_blank" href="imagery/Mistletoe-On-My-Necklace.jpg">
+            <img src="imagery/Mistletoe-On-My-Necklace.jpg" alt="Mistletoe-On-My-Necklace">
+          </a>
+          <div class="desc">Mistletoe Noose</div>
+        </div>
+      </div>
+    </div><!-- galleryContain END -->
+    <!--
+       ____            _             _   
+      / ____|          | |           | |  
+     | |     ___  _ __ | |_ ___ _ __ | |_ 
+     | |    / _ \| '_ \| __/ _ \ '_ \| __|
+     | |___| (_) | | | | ||  __/ | | | |_ 
+      \_____\___/|_| |_|\__\___|_| |_|\__|
+    -->
+    <div class="whiteArea">
+    <div class="mainContent">
+      <div class="headline">
+        <h3>Purchase Season Tickets Now!</h3>
+        <br>
+        <h3>Save $22.00. Four Shows for $170.00 now until June 30, 2017.<br>Call (502) 538-4100 or (502) 426-7100</h3>
+        <br>
+        <h3>Check Out Next Season!</h3>
+      </div><!-- headline END -->
+
+      <div class="content">
+        <div class="asideImage"><img src="Imagery/card_game.jpg" alt="card game"></div>
+        <p>
+        <div class="paragraph">WhoDunnit shows are original, lively and thoroughly enertaining, as well as a mystery lover's dream!
+        <p>
+        Clues are cleverly included in the scripted action, and in between scenes, as the audience enjoys a fine meal, the actors mingle with the audience in character, allowing guests to ask questions and get more clues. If you ask them if they are the killer, they will all say no, but that is the only time they are permitted to lie. To all other questions, they must either evade the question or tell you the truth.
+        <p>
+        Audiences love interacting with the characters and trying to figure out who dunnit!</div><!-- paragraph END-->
+        </div>
+    </div><!-- mainContent END -->
+    </div><!-- whiteArea END -->
+    
+  		</div>
+
+  		<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  		<script src="javascript.js"></script>
+  	</body>
+</html>
---
+++ whodunnit/inlcudes/_navHead.php
@@ -0,0 +1,50 @@
+    <!--
+      _   _             
+     | \ | |            
+     |  \| | __ ___   __
+     | . ` |/ _` \ \ / /
+     | |\  | (_| |\ V / 
+     |_| \_|\__,_| \_/  
+    -->
+    <div class="purpleBox">
+      <div class="topNav">
+      <ul>
+        <li><a href="sponsorship.html" style="text-decoration:none">Sponsorship</a></li>
+        <li><a href="aboutus.html" style="text-decoration:none">About Us</a></li>
+      </ul>
+      </div>
+    </div><!-- purpleBox END -->
+
+
+    <!--
+     _    _                _           
+    | |  | |              | |          
+    | |__| | ___  __ _  __| | ___ _ __ 
+    |  __  |/ _ \/ _` |/ _` |/ _ \ '__|
+    | |  | |  __/ (_| | (_| |  __/ |   
+    |_|  |_|\___|\__,_|\__,_|\___|_|
+    -->
+    <div class="headerArea">
+      <a href="home.html"><img src="imagery/WhoDunnit_logo.png" alt="WhoDunnits"></a>
+
+      <h4>Kentuckiana's Premier Murder Mystery Theatre Group</h4>
+    </div><!-- headerArea END -->
+
+    <!--
+      _   _             
+     | \ | |            
+     |  \| | __ ___   __
+     | . ` |/ _` \ \ / /
+     | |\  | (_| |\ V / 
+     |_| \_|\__,_| \_/  
+    -->
+    <div class="purpleBox">
+      <div class="bottomNav">
+      <ul>
+        <li><a href="news_events.html" style="text-decoration:none">News &amp; Events</a></li>
+        <li><a href="privateparties.html" style="text-decoration:none">Private Parties</a></li>
+        <li><a href="directions.html" style="text-decoration:none">Directions</a></li>
+      </ul>
+      </div>
+      <div class="footer"><div class="boxes"><h5>Presenting original, interactive murder mysteries to delight and entertain!</h5></div></div>
+    </div><!-- purpleBox END -->
